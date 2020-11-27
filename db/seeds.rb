6238e29b5d65e1272608e74abbd7141bdef2ae1b
Guilherme Almeida <guilherme.almeida@enap.gov.br>
2020-11-27 18:26:58 -0300

    seed made. controllers generated. pending routes, views, controllers functions'

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Destruindo todos os registros de restaurante'
+Restaurant.destroy_all
+puts 'concluído'
+puts 'Destruindo todos os registros de reviews'
+Review.destroy_all
+puts 'concluído'
+
+puts 'vai começar o seed de restaurantes'
+
+100.times do
+  new_restaurant = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: %w[chinese italian japanese french belgian].sample
+  )
+  puts "restaurante criado - #{new_restaurant.name}  - Endereço #{new_restaurant.address}  - fone #{new_restaurant.phone_number} - categoria #{new_restaurant.category}"
+end
+
+puts 'criou todos os restaurantes'
+
+puts 'vai começar o seed de reviews'
+
+500.times do
+  new_review = Review.create(
+    content: Faker::Restaurant.review,
+    rating: rand(0..5),
+    restaurant_id: rand(Restaurant.first.id..Restaurant.last.id)
+  )
+
+  puts "review criado - #{new_review.content}   - rating #{new_review.rating}   - restaurant_id #{new_review.restaurant_id}"
+end
