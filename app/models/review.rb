8a78b2093d4690bf4bd7ef627cc216d64008c89b
Guilherme Almeida <guilherme.almeida@enap.gov.br>
2020-11-27 20:51:51 -0300

    finally working!, could work more on css

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,8 +4,7 @@ class Review < ApplicationRecord
   validates :restaurant, presence: true
   validates :rating, :content, presence: true
   validates :rating, numericality: { only_integer: true }
-  validates :rating, numericality: { greater_than_or_equal_to: 0 }
-  validates :rating, numericality: { less_than_or_equal_to: 5 }
+  validates :rating, inclusion: { in: 0..5 }
 
   # criar controller review
   # criar controller restaurant
