a9b3f683686222b4f78fbab1536f58093ad7f8ba
Guilherme Almeida <guilherme.almeida@enap.gov.br>
2020-11-27 17:19:06 -0300

    all specs green, controllers missing

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,6 @@
 class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+
+  validates :name, :address, :phone_number, :category, presence: true
+  validates :category, inclusion: { in: %w[chinese italian japanese french belgian], message: 'This is not a valid category' }
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,14 @@
 class Review < ApplicationRecord
+  belongs_to :restaurant
+
+  validates :restaurant, presence: true
+  validates :rating, :content, presence: true
+  validates :rating, numericality: { only_integer: true }
+  validates :rating, numericality: { greater_than_or_equal_to: 0 }
+  validates :rating, numericality: { less_than_or_equal_to: 5 }
+
+  # criar controller review
+  # criar controller restaurant
+  # mudar campo telefone do restaurant para string
+  # criar campo restaurant_id no review
 end
